94f5d254874f3b5d095a71beb593fdeda0cd8af8
marcinpank <marcin.pankiewicz@gmail.com>
2020-06-07 16:00:03 +0200

    Added login form authenticator, imporvement bese html twig

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -31,6 +32,6 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-        throw new \Exception('Will be intercepted before getting here');
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
